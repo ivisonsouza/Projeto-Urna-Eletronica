5c2ef455a8204e912b0cbfc6afd52972fe05c06a
ivison <ivisonsouza@hotmail.com>
2022-09-29 21:07:09 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,68 @@
+let etapas = [
+
+   
+    {
+        titulo: 'DEPUTADO FEDERAL',
+        numero: 4,
+        candidatos: [
+            {
+                numero: '9876',
+                nome: 'Fulano',
+                partido: 'PT',
+                fotos: [ {url: 'fulano.png', legenda: 'Deputado'} ]
+            },
+            {
+                numero: '1234',
+                nome: 'Cicrano',
+                partido: 'PL',
+                fotos: [ {url: 'fulano.png', legenda: 'Deputado'} ]
+            },
+            {
+                numero: '4321',
+                nome: 'Beltrano',
+                partido: 'União',
+                fotos: [ {url: 'fulano.png', legenda: 'Deputado'} ]
+            },
+        ],   
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numero: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'Lula',
+                partido: 'PT',
+                fotos: [  {url: './img/13.jpg', legenda: 'Presidente'} ]     
+                
+            },
+            {
+                numero: '25',
+                nome: 'Ivison',
+                partido: 'DEM',
+                fotos: [  {url: './img/25.jpg', legenda: 'Presidente'} ]     
+                
+            },
+            {
+                numero: '12',
+                nome: 'Ciro Gomes',
+                partido: 'PDT',
+                fotos: [  {url: './img/12.jpg', legenda: 'Presidente'} ]
+            },
+            {
+                numero: '15',
+                nome: 'Simone Tebet',
+                partido: 'MDB',
+                fotos: [ {url: './img/15.png', legenda: 'Presidente'} ]
+            },
+            {
+                numero: '22',
+                nome: 'Jair Bolsonaro',
+                partido: 'PL',
+                fotos: [ {url: './img/22.jpg', legenda: 'Presidente'} ]
+            },
+        ],
+
+    },
+
+];
